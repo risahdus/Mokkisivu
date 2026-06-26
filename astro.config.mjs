36c8851e3479ff436ec429f0c9252e72cb730eea
Kristian Vaisanen <kristian.vaisanen@bevenic.com>
2026-06-26 19:30:02 +0300

    update node js

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    // Pakotetaan Vercel-funktio käyttämään Node 20:tä, jotta vältytään Node 18 -virheeltä
+    runtime: 'nodejs20.x'
+  }),
 });
